69161ff0488c2ce948579ec022e7c17b805d43b4
Pons Emilien <emilien.pons@etu.iut-tlse3.fr>
2026-06-19 18:12:19 +0200

    button ?

--- src/components/button.ts
+++ src/components/button.ts
@@ -4,12 +4,12 @@ import { mergeClass } from '../utils/merge-class';
 
 export interface ButtonProps extends AtlasTraits<'button'> {
     variant?: 'default' | 'secondary' | 'outline' | 'ghost' | 'destructive' | 'link';
-    size?: 'default' | 'sm' | 'lg' | 'icon';
+    size?: 'md' | 'sm' | 'lg' | 'icon';
 }
 
 export const Button = ({
                            variant = 'default',
-                           size = 'default',
+                           size = 'md',
                            className,
                            ...props
                        }: ButtonProps) => {
@@ -29,7 +29,7 @@ export const Button = ({
 
 
             {
-                default: 'h-9 px-4 py-2',
+                md: 'h-9 px-4 py-2',
                 sm: 'h-8 rounded-md px-3 text-xs',
                 lg: 'h-10 rounded-md px-8',
                 icon: 'h-9 w-9'
